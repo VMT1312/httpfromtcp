e89a84a0050760adfee5fb00b19ab884b1a9b03a
VMT1312 <thiemmavinh123@gmail.com>
2026-01-10 14:45:52 +0800

    channel refactor

--- main.go
+++ main.go
@@ -13,31 +13,45 @@ func main() {
 	if err != nil {
 		log.Printf("Error open file: %v", err)
 	}
-	defer f.Close()
 
-	var line string
-	for {
-		b := make([]byte, 8)
-		n, err := f.Read(b)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			log.Printf("Error reading file: %v", err)
-			break
-		}
-		txt := string(b[:n])
-		parts := strings.Split(txt, "\n")
-		parts_len := len(parts)
-		for i := 0; i < parts_len-1; i++ {
-			part := parts[i]
-			complete_line := line + part
-			fmt.Printf("read: %s\n", complete_line)
-			line = ""
-		}
-		line += parts[parts_len-1]
-	}
-	if line != "" {
+	ch := getLinesChannel(f)
+	for line := range ch {
 		fmt.Printf("read: %s\n", line)
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func(f io.ReadCloser) {
+		defer f.Close()
+		defer close(ch)
+		var line string
+		for {
+			b := make([]byte, 8)
+			n, err := f.Read(b)
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				log.Printf("Error reading file: %v", err)
+				break
+			}
+			txt := string(b[:n])
+			parts := strings.Split(txt, "\n")
+			parts_len := len(parts)
+			for i := 0; i < parts_len-1; i++ {
+				part := parts[i]
+				complete_line := line + part
+				ch <- complete_line
+				line = ""
+			}
+			line += parts[parts_len-1]
+		}
+		if line != "" {
+			ch <- line
+		}
+	}(f)
+
+	return ch
 }
