64f13bf08f4ff23549b27176a82742b0ab24ee7a
VMT1312 <thiemmavinh123@gmail.com>
2026-01-25 17:16:06 +0800

    fix: correct output formatting for request version and ensure headers are displayed

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -31,7 +31,11 @@ func main() {
 		fmt.Println("Request line:")
 		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
 		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
-		fmt.Printf("- Version: %s", r.RequestLine.HttpVersion)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
+		fmt.Println("Headers:")
+		for key, value := range r.Headers {
+			fmt.Printf("- %s: %s\n", key, value)
+		}
 	}
 
 }
